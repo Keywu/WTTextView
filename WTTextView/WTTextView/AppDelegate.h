e7952b35b747204328218252df168312616d8522
Yandere <>
2018-03-27 17:49:58 +0800

    Initial commit

---
+++ WTTextView/WTTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WTTextView
+//
+//  Created by macmini on 2018/3/27.
+//  Copyright © 2018年 org.objc.yandere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WTTextView/WTTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WTTextView
+//
+//  Created by macmini on 2018/3/27.
+//  Copyright © 2018年 org.objc.yandere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
